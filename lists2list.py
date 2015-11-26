0fd21cdb5026964eb544174bb811b6ee0abf86a7
isrusin <is_rusinov@mail.ru>
2015-11-26 17:43:34 +0300

    lists2list: add verbose mode; checked

--- lists2list.py
+++ lists2list.py
@@ -8,9 +8,12 @@ def symmetric_difference_update(fset, sset):
 	fset.symmetric_difference_update(sset)
 	fset.difference_update(toremove)
 
-def read_acvs(inlist):
+def read_acvs(inlist, verbose):
 	with inlist:
-		return set(inlist.read().strip().split('\n'))
+		idset = set(inlist.read().strip().split('\n'))
+		if verbose:
+			sys.stderr.write("%s: %d\n" % (inlist.name, len(idset)))
+		return idset
 
 if __name__ == "__main__":
 	parser = ap.ArgumentParser(description="Get ID-sets and return their " +
@@ -29,19 +32,27 @@ if __name__ == "__main__":
 	parser.add_argument("-o", "--output", dest="oulist", metavar="FILE",
 	                    type=ap.FileType('w'), default=sys.stdout,
 	                    help="output file, default is stdout")
+	parser.add_argument("-v", "--verbose", dest="verbose", action="store_true",
+	                    help="verbose mode, print every set size to stderr")
 	args = parser.parse_args()
 	action = {'u': set.update,
 	          'i': set.intersection_update,
 	          'd': set.difference_update,
 	          's': symmetric_difference_update
 	         }.get(args.action, set.update)
-	current_set = read_acvs(args.sets.pop(0))
+	current_set = read_acvs(args.sets.pop(0), args.verbose)
 	for next_set in args.sets:
-		action(current_set, read_acvs(next_set))
+		action(current_set, read_acvs(next_set, args.verbose))
+	printed = 0
 	with args.oulist as oulist:
+		if args.verbose:
+			sys.stderr.write("%s: %d\n" % (oulist.name, len(current_set)))
 		for line in sorted(current_set):
 			try:
 				oulist.write(line + '\n')
+				printed += 1
 			except IOError:
+				if args.verbose:
+					sys.stderr.write("printed less than %d IDs!\n" % printed)
 				break
 
