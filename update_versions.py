cfb47c5f5d67402f217e23a610d1293f5956567a
isrusin <is_rusinov@mail.ru>
2016-06-02 13:15:12 +0300

    update_versions: add 'delimited' option

--- update_versions.py
+++ update_versions.py
@@ -21,6 +21,11 @@ if __name__ == "__main__":
             "-s", dest="source", metavar="LIST.acv", type=ap.FileType("r"),
             required=True, help="ACv list to get versions from"
             )
+    parser.add_argument(
+            "-d", "--delimiter", dest="delimiter", metavar="STRING",
+            nargs="?", const=",", help="""switch to 'delimited' mode and
+            set up the delimiter, default is coma (-d without value)"""
+            )
     ougroup = parser.add_mutually_exclusive_group()
     ougroup.add_argument(
             "-i", "--in-place", dest="inplace", action="store_true",
@@ -31,7 +36,6 @@ if __name__ == "__main__":
             default=sys.stdout, help="output file, default stdout"
             )
     args = parser.parse_args()
-    keep = False
     ac2acv = dict()
     with args.source as source:
         for line in source:
@@ -39,17 +43,23 @@ if __name__ == "__main__":
             ac = acv.split(".")[0]
             ac2acv[ac] = acv
     lines = []
+    index = args.column
+    delimiter = args.delimiter
     with args.infile as infile:
         if args.title:
             lines.append(infile.readline().strip())
         for line in infile:
             vals = line.strip().split("\t")
-            ac = vals[args.column].split(".")[0]
-            if ac in ac2acv:
-                vals[args.column] = ac2acv[ac]
-                lines.append("\t".join(vals))
-            elif keep:
-                lines.append("\t".join(vals))
+            val = vals[index]
+            if delimiter:
+                acs = map(lambda x: x.split(".")[0], val.split(delimiter))
+                replaced = set(ac2acv.get(ac, "") for ac in acs)
+                replaced.discard("")
+                vals[index] = delimiter.join(sorted(replaced))
+            else:
+                ac = val.split(".")[0]
+                vals[index] = ac2acv.get(ac, "")
+            lines.append("\t".join(vals))
     oufile = open(infile.name, "w") if args.inplace else args.oufile
     with oufile:
         oufile.write("\n".join(lines) + "\n")
