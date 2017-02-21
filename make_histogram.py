6059dcec1c0da72106a7a25374958f122c8daddb
isrusin <is_rusinov@mail.ru>
2017-02-21 14:48:48 +0300

    make_histogram: handle empty TSV without error; make some improvements of code style; change default Fo column

--- make_histogram.py
+++ make_histogram.py
@@ -1,7 +1,10 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
-import argparse as ap
+"""Make a histrogram of contrast ratio values."""
+
+import argparse
 import math
+import sys
 
 cmpls = {
     "A": "T", "B": "V", "T": "A", "V": "B", "W": "W",
@@ -15,40 +18,39 @@ def ds(site):
         compl = cmpls[nucl] + compl
     return min([site, compl])
 
-
-if __name__ == "__main__":
-    parser = ap.ArgumentParser(
-            description="Make histrogram of contrast ratio values."
-            )
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description="Make histrogram of contrast ratio values."
+    )
     parser.add_argument(
-            "intab", metavar="IN.tsv", type=ap.FileType("r"),
-            help="input tabular file with contrast ratio values"
-            )
+        "intab", metavar="IN.tsv", type=argparse.FileType("r"),
+        help="input tabular file with contrast ratio values"
+    )
     parser.add_argument(
-            "ouhst", metavar="OU.hst", type=ap.FileType("w"),
-            help="output histogram file"
-            )
+        "ouhst", metavar="OU.hst", type=argparse.FileType("w"),
+        help="output histogram file"
+    )
     parser.add_argument(
-            "--bins", dest="bins_number", type=int, default=41,
-            help="number of bins, default 41"
-            )
+        "--bins", dest="bins_number", type=int, default=41,
+        help="number of bins, default 41"
+    )
     parser.add_argument(
-            "--cutoff", dest="cutoff", type=float, default=15,
-            help="expected number cutoff, default 15"
-            )
+        "--cutoff", dest="cutoff", type=float, default=15,
+        help="expected number cutoff, default 15"
+    )
     parser.add_argument(
-            "--expected", metavar="N", dest="exp", type=int, default=-3,
-            help="index of expected number column, default -3"
-            )
+        "--expected", metavar="N", dest="exp", type=int, default=-3,
+        help="index of expected number column, default -3"
+    )
     parser.add_argument(
-            "--observed", metavar="N", dest="obs", type=int,
-            help="index of observed number column, default --expected - 1"
-            )
-    args = parser.parse_args()
+        "--observed", metavar="N", dest="obs", type=int, default=2,
+        help="index of observed number column, default 2"
+    )
+    args = parser.parse_args(argv)
     bins = args.bins_number
     bins_total = bins + 1 # + '> 2.0'
     exp_index = args.exp
-    obs_index = args.obs or args.exp - 1
+    obs_index = args.obs
     counts = dict()
     with args.intab as intab:
         intab.readline()
@@ -75,7 +77,12 @@ if __name__ == "__main__":
         bin_index = min(int(ratio * bins / span), bins)
         hist[bin_index] += 1
     total = sum(hist)
+    sys.stderr.write("The histogram was build by %d values.\n" % total)
+    if not total:
+        total = 1
     with args.ouhst as ouhst:
         for label, value in zip(labs, hist):
             ouhst.write("%s\t%.2f\n" % (label, value * 100.0 / total))
 
+if __name__ == "__main__":
+    sys.exit(main())
