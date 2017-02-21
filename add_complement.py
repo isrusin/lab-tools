cbc1641e9829740d285d11448f7264f12943468a
isrusin <is_rusinov@mail.ru>
2017-02-21 14:53:38 +0300

    add_complement: add ability of processing of TSV files

--- add_complement.py
+++ add_complement.py
@@ -1,15 +1,16 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 """Add or remove complement sites."""
 
-import argparse as ap
-import signal as sg
+import argparse
+import signal
 import sys
 
 _ADD = 0
 _REVERSE = 1
 _WATSON = 2
 _CRICK = 3
+_ISPAL = 4
 
 COMPLS = {
     "A": "T", "T": "A", "C": "G", "G": "C",
@@ -20,73 +21,118 @@ COMPLS = {
 def get_site_versions(site):
     """Get all versions of double-stranded site."""
     rsite = "".join([COMPLS.get(nucl, "?") for nucl in site[::-1]])
-    return site, rsite, min(site, rsite), max(site, rsite)
+    watson = min(site, rsite)
+    crick = max(site, rsite)
+    ispal = watson == crick
+    return site, rsite, watson, crick, ispal
 
-def transform_stl(instl, action, sort):
+def transform_tsv(intsv, action, sort=True, keep_repeats=False,
+                  site_index=0, id_index=None, has_title=False):
     """Read and transform site list from file-like object."""
-    sites = []
-    for line in instl:
-        versions = get_site_versions(line.strip())
-        sites.append(versions[action])
-        if action == _ADD and versions[_WATSON] != versions[_CRICK]:
-            sites.append(versions[_REVERSE])
+    if has_title:
+        title = intsv.readline()
+    lines = []
+    uniqs = set()
+    split_num = max(site_index, id_index) + 1
+    for line in intsv:
+        vals = line.strip().split("\t", split_num)
+        site = vals[site_index]
+        versions = get_site_versions(site)
+        if id_index is None:
+            uid = tuple(vals[:site_index] + vals[site_index+1:])
+        else:
+            uid = vals[id_index]
+        uids = (versions[_WATSON], uid)
+        if not keep_repeats:
+            if uids in uniqs:
+                continue
+            uniqs.add(uids)
+        vals[site_index] = versions[action]
+        lines.append(vals)
+        if action == _ADD and not versions[_ISPAL]:
+            rvals = vals[:]
+            rvals[site_index] = versions[_REVERSE]
+            lines.append(rvals)
     if sort:
-        sites = sorted(set(sites))
-    return sites
+        lines = sorted(lines, key=lambda x: x[site_index])
+    lines = ["\t".join(line) + "\n" for line in lines]
+    if has_title:
+        lines.insert(0, title)
+    return lines
 
-def main(args=None):
+def main(argv=None):
     """Parse arguments and call transform_stl."""
-    parser = ap.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description="Add or remove complement sites."
     )
     parser.add_argument(
-        "instl", metavar="IN_LIST", type=ap.FileType("r"),
-        help="input site list, use '-' for stdin"
+        "intsv", metavar="FILE", type=argparse.FileType("r"),
+        help="input table with sites, use '-' for STDIN"
     )
     out_group = parser.add_mutually_exclusive_group(required=False)
     out_group.add_argument(
-        "-o", dest="oustl", metavar="OU_LIST", type=ap.FileType("w"),
-        default=sys.stdout, help="output site list, default stdout"
+        "-o", dest="outsv", metavar="FILE", type=argparse.FileType("w"),
+        default=sys.stdout, help="output site list, default is STDOUT"
     )
     out_group.add_argument(
         "-i", "--in-place", dest="inplace", action="store_true",
-        help="act in-place, stdout in case of stdin"
+        help="act in-place, STDOUT in case of STDIN"
     )
     parser.add_argument(
-        "-k", "--keep-order", dest="sort", action="store_false",
-        help="keep order of sites, do not sort output"
+        "-t", "--title", dest="has_title", action="store_true",
+        help="treat the first line of the input as a title (keep it)"
+    )
+    parser.add_argument(
+        "-k", "--keep-order", dest="keep_order", action="store_true",
+        help="keep order of lines, do not sort the output"
+    )
+    parser.add_argument(
+        "-K", "--keep-repeats", dest="keep_repeats", action="store_true",
+        help="keep repeated lines and their order"
+    )
+    parser.add_argument(
+        "-s", "--site-index", dest="site_index", metavar="N", type=int,
+        default=0, help="index of the site column, default 0"
+    )
+    parser.add_argument(
+        "-u", "--uid-index", dest="id_index", metavar="N", type=int,
+        help="index of the ID column, entire line is used as ID by default"
     )
     act_group = parser.add_mutually_exclusive_group(required=False)
     act_group.add_argument(
-        "-a", "--add", dest="action", action="store_const",
+        "-A", "--add", dest="action", action="store_const",
         const=_ADD, help="add complementary sites to the list, default"
     )
     act_group.add_argument(
-        "-r", "--reverse", dest="action", action="store_const",
+        "-R", "--reverse", dest="action", action="store_const",
         const=_REVERSE, help="replace each site with complementary one"
     )
     act_group.add_argument(
-        "-w", "--watson", dest="action", action="store_const",
-        const=_WATSON, help="return only the first (alphabetically)" +
-        " site for each complementary pair"
+        "-W", "--watson", dest="action", action="store_const",
+        const=_WATSON, help="""return only the first (alphabetically)
+        site for each complementary pair"""
     )
     act_group.add_argument(
-        "-c", "--crick", dest="action", action="store_const",
-        const=_CRICK, help="return only the last (alphabetically)" +
-        " site for each complementary pair"
+        "-C", "--crick", dest="action", action="store_const",
+        const=_CRICK, help="""return only the last (alphabetically)
+        site for each complementary pair"""
     )
-    params = parser.parse_args(args)
-    action = params.action or _ADD
-    sites = []
-    with params.instl as instl:
-        sites = transform_stl(instl, action, params.sort)
-    oustl = params.oustl
-    if params.inplace and instl.name != "<stdin>":
-        oustl = open(instl.name, "w")
-    with oustl:
-        if oustl.name == "<stdout>":
-            sg.signal(sg.SIGPIPE, sg.SIG_DFL)
-        oustl.write("\n".join(sites) + "\n")
+    args = parser.parse_args(argv)
+    keep_repeats = args.keep_repeats
+    sort = not (args.keep_order or keep_repeats)
+    with args.intsv as intsv:
+        lines = transform_tsv(
+            intsv, args.action or _ADD, has_title=args.has_title,
+            sort=sort, keep_repeats=keep_repeats,
+            site_index=args.site_index, id_index=args.id_index
+        )
+    outsv = args.outsv
+    if args.inplace and intsv.name != "<stdin>":
+        outsv = open(intsv.name, "w")
+    with outsv:
+        if outsv.name == "<stdout>":
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+        outsv.writelines(lines)
 
 if __name__ == "__main__":
     sys.exit(main())
