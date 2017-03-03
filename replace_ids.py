a99dad94f8f8ec5e835b805fb0922b5cce5a15ff
isrusin <is_rusinov@mail.ru>
2017-03-03 23:50:48 +0300

    [replace_ids] Some corrections of syntax and code style.
    * Correct identation in the CLI section.
    * Wrap the code in main function call.
    * Remove abbrs of the imports.

--- replace_ids.py
+++ replace_ids.py
@@ -1,60 +1,63 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import argparse as ap
-import signal as sg
+"""Replace IDs by dictionary."""
+
+import argparse
+import signal
 import sys
 
-if __name__ == "__main__":
-    sg.signal(sg.SIGPIPE, sg.SIG_DFL)
-    parser = ap.ArgumentParser(description="Replace IDs by dictionary.")
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description="Replace IDs by dictionary."
+    )
     parser.add_argument(
-            "infile", metavar="FILE", type=ap.FileType("r"),
-            help="input file with IDs column, which could be delimited"
-            )
+        "infile", metavar="FILE", type=argparse.FileType("r"),
+        help="input file with IDs column, which could be delimited"
+    )
     parser.add_argument(
-            "-s", dest="indct", type=ap.FileType("r"), metavar="DICT",
-            required=True, help="""source .dct file with IDs mapping;
-            it should be tab-separated untitled file with IDs in the
-            first column, line residuals after the first tab will be
-            treated as new IDs (should not be unique actually and could
-            contain any non-line-breaking symbols, including spaces and
-            tabs)"""
-            )
+        "-s", dest="indct", metavar="DICT", type=argparse.FileType("r"),
+        required=True, help="""source .dct file with IDs mapping;
+        it should be tab-separated untitled file with IDs in the
+        first column, line residuals after the first tab will be
+        treated as new IDs (should not be unique actually and could
+        contain any non-line-breaking symbols, including spaces and
+        tabs)"""
+    )
     parser.add_argument(
-            "-o", "--out", dest="oufile", metavar="FILE",
-            type=ap.FileType("w"), default=sys.stdout,
-            help="output file, default stdout"
-            )
+        "-o", "--out", dest="oufile", metavar="FILE",
+        type=argparse.FileType("w"), default=sys.stdout,
+        help="output file, default STDOUT"
+    )
     parser.add_argument(
-            "-c", "--column", dest="index", type=int, default=0,
-            metavar="N", help="index (from 0) of IDs column, default 0"
-            )
+        "-c", "--column", dest="index", metavar="N", type=int,
+        default=0, help="index (from 0) of IDs column, default 0"
+    )
     parser.add_argument(
-            "-t", "--title", dest="title", action="store_true",
-            help="treat the first line of the input file as title line"
-            )
+        "-t", "--title", dest="title", action="store_true",
+        help="treat the first line of the input file as title line"
+    )
     parser.add_argument(
-            "-d", "--delimiter", dest="delimiter", metavar="STRING",
-            nargs="?", default=",", help="""set up the delimiter for
-            multiple-entry values, default is coma; -d with no value means
-            not to split values"""
-            )
+        "-d", "--delimiter", dest="delimiter", metavar="STRING",
+        nargs="?", default=",", help="""set up the delimiter for
+        multiple-entry values, default is coma; -d with no value means
+        not to split values"""
+    )
     parser.add_argument(
-            "-k", "--keep-order", dest="keep_order", action="store_true",
-            help="""keep order of IDs, meaningless with -d without value;
-            default behaviour is to sort new IDs and remove duplicates"""
-            )
+        "-k", "--keep-order", dest="keep_order", action="store_true",
+        help="""keep order of IDs, meaningless with -d without value;
+        default behaviour is to sort new IDs and remove duplicates"""
+    )
     parser.add_argument(
-            "-e", "--keep-empty", dest="keep_empty", action="store_true",
-            help="""keep lines with empty (after ID replacement) ID cell,
-            default behaviour is to skip them"""
-            )
+        "-e", "--keep-empty", dest="keep_empty", action="store_true",
+        help="""keep lines with empty (after ID replacement) ID cell,
+        default behaviour is to skip them"""
+    )
     parser.add_argument(
-            "-m", "--missed", dest="missed", metavar="STRING", nargs="?",
-            default="", help="""a string to replace IDs missed in source
-            with; -m without value forces to keep missed IDs in place;
-            default behaviour (without -m) is to remove all missed IDs"""
-            )
+        "-m", "--missed", dest="missed", metavar="STRING", nargs="?",
+        default="", help="""a string to replace IDs missed in source
+        with; -m without value forces to keep missed IDs in place;
+        default behaviour (without -m) is to remove all missed IDs"""
+    )
     args = parser.parse_args()
     idict = dict()
     with args.indct as indct:
@@ -73,6 +76,8 @@ if __name__ == "__main__":
     if missed is None:
         replace = lambda x: idict.get(x, x)
     with args.infile as infile, args.oufile as oufile:
+        if oufile.name == "<stdout>":
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
         if args.title:
             oufile.write(infile.readline())
         for line in infile:
@@ -85,4 +90,7 @@ if __name__ == "__main__":
             vals[index] = (delimiter or "").join(replaced)
             if vals[index] or keep_empty:
                 oufile.write("\t".join(vals) + "\n")
+
+if __name__ == "__main__":
+    sys.exit(main())
 
