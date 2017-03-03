d631104b86d1554e1fc8620fa2bba5f88c56489f
isrusin <is_rusinov@mail.ru>
2017-03-04 01:11:23 +0300

    [regenerate_sites] Some syntax corrections and code style improvements.
    * Correct indentation in the CLI section.
    * Wrap the code in main function call.
    * Remove abbrs of the imports.
    * Add conditional SIGPIPE treatment.

--- regenerate_sites.py
+++ regenerate_sites.py
@@ -1,58 +1,62 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
-import argparse as ap
+"""Make list of non-degenerate variants of given degenerate sites."""
+
+import argparse
+import signal
 import sys
 
-nucls = {
-        "N": ["A", "C", "G", "T"],
-        "A": ["A"], "B": ["C", "G", "T"],
-        "C": ["C"], "D": ["A", "G", "T"],
-        "G": ["G"], "H": ["A", "C", "T"],
-        "T": ["T"], "V": ["A", "C", "G"],
-        "M": ["A", "C"], "K": ["G", "T"],
-        "R": ["A", "G"], "Y": ["C", "T"],
-        "W": ["A", "T"], "S": ["C", "G"]
-        }
+NUCLS = {
+    "N": ["A", "C", "G", "T"],
+    "A": ["A"], "B": ["C", "G", "T"],
+    "C": ["C"], "D": ["A", "G", "T"],
+    "G": ["G"], "H": ["A", "C", "T"],
+    "T": ["T"], "V": ["A", "C", "G"],
+    "M": ["A", "C"], "K": ["G", "T"],
+    "R": ["A", "G"], "Y": ["C", "T"],
+    "W": ["A", "T"], "S": ["C", "G"]
+}
 
 def regenerate(dsite):
+    """Get non-degenerate version of the site."""
     regenerated = [""]
     for dnucl in dsite:
         elongated = []
-        for nucl in nucls[dnucl]:
+        for nucl in NUCLS[dnucl]:
             for primer in regenerated:
                 elongated.append(primer + nucl)
         regenerated = elongated
     return regenerated
 
-
-if __name__ == "__main__":
-    parser = ap.ArgumentParser(
-            description="""Make list of non-degenerate variants of a
-            degenerate sites."""
-            )
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description="Make list of non-degenerate variants of given sites."
+    )
     parser.add_argument(
-            "-i", metavar="FILE", dest="instl", type=ap.FileType("r"),
-            default=sys.stdin, help="input .stl file, default stdin"
-            )
+        "instl", metavar="FILE", type=argparse.FileType("r"),
+        default=sys.stdin, help="input .stl file, use '-' for STDIN"
+    )
     parser.add_argument(
-            "-o", metavar="FILE", dest="oustl", type=ap.FileType("w"),
-            default=sys.stdout, help="output .stl file, default stdout"
-            )
+        "-o", metavar="FILE", dest="oustl", type=argparse.FileType("w"),
+        default=sys.stdout, help="output .stl file, default STDOUT"
+    )
     parser.add_argument(
-            "-k", "--keep", dest="keep", action="store_true",
-            help="keep degenerate sites in the list, default replacement"
-            )
-    args = parser.parse_args()
+        "-k", "--keep", dest="keep", action="store_true",
+        help="keep degenerate sites in the list, default replacement"
+    )
+    args = parser.parse_args(argv)
     with args.instl as instl:
-        dsites = set(instl.read().strip().split("\n"))
+        dsites = set(instl.read().strip().split())
     sites = set()
     for dsite in dsites:
         sites.update(regenerate(dsite))
     if args.keep:
         sites.update(dsites)
     with args.oustl as oustl:
-        try:
-            oustl.write("\n".join(sorted(sites)) + "\n")
-        except IOError:
-            sys.stderr.write("Partial output!\n")
+        if oustl.name == "<stdout>":
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+        oustl.write("\n".join(sorted(sites)) + "\n")
+
+if __name__ == "__main__":
+    sys.exit(main())
 
