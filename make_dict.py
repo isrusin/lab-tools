bce6f47176bec54070969f540b7d1c7ef49469f1
isrusin <is_rusinov@mail.ru>
2017-03-04 00:32:31 +0300

    [make_dict] Some corrections of syntax and code style.
    * Correct identation in the CLI section.
    * Wrap the code in main function call.
    * Remove abbrs of the imports.
    * Make SIGPIPE treatment conditional: only with STDOUT.

--- make_dict.py
+++ make_dict.py
@@ -1,44 +1,44 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
-import argparse as ap
-import signal as sg
+"""Transform TSV with delimited column into dictionary file."""
+
+import argparse
+import signal
 import sys
 
-if __name__ == "__main__":
-    sg.signal(sg.SIGPIPE, sg.SIG_DFL)
-    parser = ap.ArgumentParser(
-            description="Transform .tsv file with delimited column " +
-            "into .dict file."
-            )
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        description="Transform TSV with delimited column into .dct file."
+    )
     parser.add_argument(
-            "intsv", metavar="IN.tsv", type=ap.FileType("r"),
-            help="input .tsv file"
-            )
+        "intsv", metavar="FILE", type=argparse.FileType("r"),
+        help="input .tsv file"
+    )
     parser.add_argument(
-            "-t", dest="title", action="store_true",
-            help="use if input file contains title line"
-            )
+        "-t", "--title", dest="title", action="store_true",
+        help="use if input file contains title line"
+    )
     parser.add_argument(
-            "-K", dest="key_index", metavar="N", type=int, required=True,
-            help="index of the delimited 'key' column (from 0)"
-            )
+        "-K", dest="key_index", metavar="N", type=int, required=True,
+        help="index of the delimited 'key' column (from 0)"
+    )
     parser.add_argument(
-            "-d", dest="delimiter", metavar="CHAR", default=",",
-            help="inner delimiter of the column, default comma"
-            )
+        "-d", dest="delimiter", metavar="CHAR", default=",",
+        help="inner delimiter of the column, default comma"
+    )
     parser.add_argument(
-            "-s", dest="skip", metavar="STRING",
-            help="which 'key' column values to skip"
-            )
+        "-s", "--skip", dest="skip", metavar="STRING",
+        help="which 'key' column values to skip"
+    )
     parser.add_argument(
-            "-V", dest="value_index", metavar="N", type=int, default=0,
-            help="index of 'value' column, default 0"
-            )
+        "-V", dest="value_index", metavar="N", type=int, default=0,
+        help="index of 'value' column, default 0"
+    )
     parser.add_argument(
-            "-o", dest="oudct", metavar="OUT.dict", type=ap.FileType("w"),
-            default=sys.stdout, help="output .dict file, default stdout"
-            )
-    args = parser.parse_args()
+        "-o", dest="oudct", metavar="DICT", type=argparse.FileType("w"),
+        default=sys.stdout, help="output .dct file, default STDOUT"
+    )
+    args = parser.parse_args(argv)
     result = dict()
     with args.intsv as intsv:
         if args.title:
@@ -58,6 +58,11 @@ if __name__ == "__main__":
                         sys.stderr.write("identical values.\n")
                 result[key] = value
     with args.oudct as oudct:
+        if oudct.name == "<stdout>":
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
         for item in sorted(result.items()):
             oudct.write("%s\t%s\n" % item)
+
+if __name__ == "__main__":
+    sys.exit(main())
 
