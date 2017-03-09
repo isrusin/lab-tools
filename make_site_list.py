938ba8508791ea2ee7424898633c1528915ace00
isrusin <is_rusinov@mail.ru>
2017-03-09 03:56:34 +0300

    [make_site_list] Improve code structure and style.
    * Change shebang to call env.
    * Remove name abbreviations from imports.
    * Move SIGPIPE handler out of the main function.
    * Remove excessive short functions.
    * Touch the help messages.
    * Add argv argument to the main function.
    * Wrap the main call in sys.exit().

--- make_site_list.py
+++ make_site_list.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 """Site list generator.
 
@@ -6,11 +6,12 @@ The tool generates a list of all sites of the given length or all sites
 matching the specified template.
 """
 
-import argparse as ap
+import argparse
 from itertools import product
 import re
-import signal as sg
+import signal
 import sys
+
 
 def mask_type(value):
     """Site template type."""
@@ -24,45 +25,38 @@ def mask_type(value):
     mask = re.sub(r"\d", lambda x: "{%s}" % x.group(0), mask)
     return mask, length
 
-def generate_sites(length, with_n=False):
-    """Return generator of all nucleotide combinations as tuples."""
-    return product("ACGTN" if with_n else "ACGT", repeat=length)
 
-def print_site_list(sites, oustl):
-    """Write site list into file."""
-    oustl.write("\n".join(sorted(sites)) + "\n")
-
-def main():
+def main(argv=None):
     """Main function."""
-    sg.signal(sg.SIGPIPE, sg.SIG_DFL)
-    parser = ap.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description="Make list of all sites of the given length or mask."
     )
     parser.add_argument(
-        "-o", dest="oustl", metavar="FILE", type=ap.FileType("w"),
-        default=sys.stdout, help="output .stl file, default stdout"
+        "-o", dest="oustl", metavar="FILE", type=argparse.FileType("w"),
+        default=sys.stdout, help="output file, default STDOUT"
     )
     parser.add_argument(
         "--with-n", dest="with_n", action="store_true",
-        help="""use 5-letter alphabet [A, C, G, T, N]; leading and trailing
-        N symbols will be removed if LENGTH is specified but will not with
-        --mask option"""
+        help="""use 5-letter alphabet [A, C, G, T, N]; leading and
+        trailing N symbols will be removed if LENGTH is specified,
+        but will not with --mask option"""
     )
     desc_group = parser.add_mutually_exclusive_group(required=True)
     desc_group.add_argument(
         "-m", "--mask", type=mask_type, default=(None, 0),
         help="""site template with X instead of any nucleotide
-            (e.g. XXNNXX); digit from 1 to number of 'X's means complement
-            to according 'X' position (e.g. XXNN21 is a palindrome)"""
+        (e.g. XXNNXX); digit from 1 to number of 'X's means complement
+        to according 'X' position (e.g. XXNN21 is a palindrome)"""
     )
     desc_group.add_argument(
         "length", metavar="LENGTH", type=int, choices=range(1, 11),
         nargs="?", help="site length, from 1 to 10, including both"
     )
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
     mask = args.mask[0]
     length = args.length or args.mask[1]
-    site_gen = generate_sites(length, args.with_n)
+    nucls = "ACGTN" if args.with_n else "ACGT"
+    site_gen = product(nucls, repeat=length)
     sites = set()
     if mask:
         compls = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}
@@ -74,8 +68,13 @@ def main():
     else:
         sites = set("".join(site).strip("N") for site in site_gen)
         sites.discard("")
-    with args.oustl:
-        print_site_list(sites, args.oustl)
+    with args.oustl as oustl:
+        oustl.write("\n".join(sorted(sites)) + "\n")
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    except AttributeError:
+        pass # no signal.SIGPIPE on Windows
+    sys.exit(main())
