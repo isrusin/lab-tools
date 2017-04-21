8a236d19e2e32a7e2f7d91ec03e06d209a3fecbe
isrusin <is_rusinov@mail.ru>
2017-04-21 17:45:58 +0300

    [make_histogram] Add different modes of assymetric sites treatment.
    * Add new CLI option "-m/--ds-mode".
    * Add mode to metadata of the output histogram.

--- make_histogram.py
+++ make_histogram.py
@@ -14,11 +14,9 @@ COMPLS = {
 }
 
 
-def get_watson(site):
-    compl = ""
-    for nucl in site:
-        compl = COMPLS[nucl] + compl
-    return min([site, compl])
+def get_ds_site(site):
+    rsite = "".join(COMPLS[nucl] for nucl in site[::-1])
+    return min(site, rsite), max(site, rsite)
 
 
 def main(argv=None):
@@ -50,33 +48,49 @@ def main(argv=None):
         "--observed", metavar="N", dest="obs", type=int, default=2,
         help="index of observed number column, default 2"
     )
+    parser.add_argument(
+        "-m", "--ds-mode", choices=["none", "summarize", "justify"],
+        default="summarize", help="""how to handle assymetric sites from
+        different starins, possible options are 'none' (handle each site
+        separately), 'summarize' (sum up expected and observed numbers
+        before calculation of the ratio), and 'justify' (like 'none' but
+        also consider every palindromic site twice to justify the
+        contributions of assymetric and palindromic sites) """
+    )
     args = parser.parse_args(argv)
+    ds_mode = args.ds_mode
     bins = args.bins_number
     bins_total = bins + 1 # + '> 2.0'
     exp_index = args.exp
     obs_index = args.obs
-    counts = dict()
+    hist = [0] * bins_total
+    span = 2.0 - 0.0
+    labs = ["%.2f" % ((i+0.5) * span / bins) for i in range(bins_total)]
+    waits = dict()
     with args.intab as intab:
         intab.readline()
         for line in intab:
             vals = line.strip().split("\t")
-            acv = vals[0]
-            site = get_watson(vals[1])
-            pair = (acv, site)
+            sid = vals[0]
+            site = get_ds_site(vals[1])
+            ispal = site[0] == site[1]
+            pair = (sid, site)
             expected = float(vals[exp_index])
             observed = float(vals[obs_index])
             if math.isnan(expected) or expected <= args.cutoff:
-                expected = observed = 0
-            expected_, observed_ = counts.get(pair, (0, 0))
-            expected_ += expected
-            observed_ += observed
-            counts[pair] = (expected_, observed_)
-    hist = [0] * bins_total
-    span = 2.0 - 0.0
-    labs = ["%.2f" % ((i+0.5) * span / bins) for i in range(bins_total)]
-    for expected, observed in counts.values():
-        if expected <= args.cutoff:
-            continue
+                continue
+            if ds_mode == "summarize" and not ispal:
+                observed_, expected_ = waits.get(pair, (0, 0))
+                observed += observed_
+                expected += expected_
+                waits[pair] = (observed, expected)
+            else:
+                ratio = observed / expected
+                bin_index = min(int(ratio * bins / span), bins)
+                hist[bin_index] += 1
+                if ispal and ds_mode == "justify":
+                    hist[bin_index] += 1
+    for observed, expected in waits.values():
         ratio = observed / expected
         bin_index = min(int(ratio * bins / span), bins)
         hist[bin_index] += 1
@@ -87,6 +101,7 @@ def main(argv=None):
         ouhst.write("## Source: %s\n" % args.intab.name)
         ouhst.write("## Bins number: (%d+1)\n" % args.bins_number)
         ouhst.write("## Expected cutoff: %.1f\n" % args.cutoff)
+        ouhst.write("## Assymetric handler: %s\n" % ds_mode)
         ouhst.write("## Total: %d\n" % total)
         ouhst.write("#Bin\tPercent\n")
         if not total:
